6787e9e8ece04f6c74ab4ee00f0906467aeba38d
Fuki Sato <lego.sato.4135@gmail.com>
2022-10-19 22:59:37 +0900

    完成

--- SpeedProgramingContest2022/Program.cs
+++ SpeedProgramingContest2022/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+Console.WriteLine("Start");
+for (long i = 0; i <= long.MaxValue; i++)
+{
+    var length = i.ToString().Length;
+    long result = 0;
+    foreach (var valueChar in i.ToString())
+    {
+        var value = long.Parse(valueChar.ToString());
+        result = (long)(result + Math.Pow(value, length));
+    }
+    if (i == result)
+    {
+        Console.WriteLine("Find:" + i.ToString());
+    }
+}
+Console.WriteLine("End");
