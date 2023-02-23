27e32373b22f6d09385d2e273bafca610378fbed
Fuki Sato <lego.sato.4135@gmail.com>
2023-02-23 09:01:14 +0900

    Update new version

--- SpeedProgramingContest2022/Program.cs
+++ SpeedProgramingContest2022/Program.cs
@@ -1,17 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Start");
-for (long i = 0; i <= long.MaxValue; i++)
+﻿class Program
 {
-    var length = i.ToString().Length;
-    long result = 0;
-    foreach (var valueChar in i.ToString())
+    private static DateTime t;
+
+    static void Main(string[] args)
     {
-        var value = long.Parse(valueChar.ToString());
-        result = (long)(result + Math.Pow(value, length));
+        t = DateTime.Now;
+        for (int i = 0; i <= int.MaxValue; i++)
+        {
+            me(i);
+        }
+        Console.WriteLine("End");
+        Console.ReadLine();
     }
-    if (i == result)
+
+    private static void me(int @in)
     {
-        Console.WriteLine("Find:" + i.ToString());
+        List<int> i = new List<int>();
+        int l = 1;
+        int k = 10;
+
+        while (true)
+        {
+            if (@in < k) break;
+            l++;
+            k = k * 10;
+        }
+
+        int t = @in;
+        while (k >= 1)
+        {
+            if (t >= k)
+                i.Add(t / k);
+            t = t % k;
+            k = k / 10;
+        }
+
+        int m = 0;
+        for (int n = 0; n <= i.Count - 1; n++)
+        {
+            m += P(i[n], l);
+        }
+
+        if (@in == m)
+        {
+            TimeSpan el = DateTime.Now - Program.t;
+            Console.WriteLine(m + ": Ellapsed: " + el);
+        }
+    }
+
+    private static int P(int @in, int l)
+    {
+        int o = @in;
+        for (int i = 1; i < l; i++)
+        {
+            @in = @in * o;
+        }
+        return @in;
     }
 }
-Console.WriteLine("End");
